4482f09c3453ccc1adddbcdc9501410760c4fbd6
Youssef03-html <youssgh413@gmail.com>
2025-09-28 18:41:38 +0200

    Ejercicio no avaluable 1: Tipus i Variables

---
+++ AEA1/ActivitatsNoAvaluables/VariablesTipus.js
@@ -0,0 +1,30 @@
+// EXERCICI  1:
+
+// Variables declarades i definides
+let nom = "Youssef";
+let edat = 22;
+let carnetConduir = false;
+
+// Mostro per consola les variables
+console.log("Nom: " + nom + " Edat: " + edat + " Carnet de conduir: " + carnetConduir);
+
+
+// EXERCICI 2:
+
+// Declaro i defineixo les variables
+const pi = 3.1416;
+let radi = 5;
+
+// Declaro la variable del resultat i calculo l'àrea
+let area = pi * radi * radi;
+
+// Mostro el resultat
+console.log("Àrea del cercle: " + area);
+
+// EXERCICI 3:
+
+// Declaro i defineixo una variable bigint
+let granNumero = 18457354829457352748n;
+
+// la mostro 
+console.log("Tipus de variable: " + typeof granNumero);
